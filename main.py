67d4edca37eb741e5407c0219768b0d67697d0f4
NguyenDucAnh040924 <ducanh24092k4@gmail.com>
2026-03-16 22:45:41 +0700

    input view and ouutput

---
+++ main.py
@@ -0,0 +1,29 @@
+from process import *
+
+class Input:
+    
+    viewString: str
+    
+    def __init__(self, username, reponame):
+        self.username = username
+        self.reponame = reponame
+    
+
+    # Xu ly du lieu
+    def proccessData(self):
+        self.viewString = processing_user_data(self.username, self.reponame)
+    
+    # View sau khi tra ve du lieu
+    def view(self):
+        print(self.viewString)
+
+    
+
+
+username = input("Nhap username cua ban: ")
+reponame = input("Nhap repo name cua ban: ")
+
+# set gia tri cho username va reponame
+value = Input(username,reponame)
+value.proccessData()
+value.view()
